2cc69354ff5e27c766bf6e80025d23c467cb7d29
someaddons <someaddons@github.com>
2025-08-11 18:57:23 +0200

    4.7: Fix crash on server

--- src/main/java/com/shieldmechanics/ShieldMechanicsClient.java
+++ src/main/java/com/shieldmechanics/ShieldMechanicsClient.java
@@ -3,6 +3,7 @@ package com.shieldmechanics;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 @Environment(EnvType.CLIENT)
 public class ShieldMechanicsClient implements ClientModInitializer
@@ -11,6 +12,6 @@ public class ShieldMechanicsClient implements ClientModInitializer
     @Override
     public void onInitializeClient()
     {
-
+        ClientLifecycleEvents.CLIENT_STARTED.register(a -> ShieldDataGatherer.detectItems());
     }
 }
