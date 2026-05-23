b04abcc38210435c3b3044408d8cbfced35eac51
someaddons <someaddons@github.com>
2026-05-23 15:05:33 +0200

    4.7: Fix damage blocking damage not working with neoforge's damage container api changes

--- src/main/java/com/shieldmechanics/mixin/LivingEntityHurtMixin.java
+++ src/main/java/com/shieldmechanics/mixin/LivingEntityHurtMixin.java
@@ -3,15 +3,24 @@ package com.shieldmechanics.mixin;
 import com.shieldmechanics.event.EventHandler;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.common.damagesource.DamageContainer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
+import java.util.Stack;
+
 @Mixin(LivingEntity.class)
 public class LivingEntityHurtMixin
 {
+    @Shadow
+    @Nullable
+    protected Stack<DamageContainer> damageContainers;
+
     @Inject(method = "hurt", at = @At("HEAD"))
     private void onHurt(final DamageSource damageSource, final float f, final CallbackInfoReturnable<Boolean> cir)
     {
@@ -22,6 +31,8 @@ public class LivingEntityHurtMixin
     @ModifyVariable(method = "actuallyHurt", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private float onHurt(final float dmg, final DamageSource damageSource, final float f)
     {
-        return EventHandler.onEntityHurt(damageSource, dmg, (LivingEntity) (Object) this);
+        final float newDamage = EventHandler.onEntityHurt(damageSource, damageContainers.peek().getNewDamage(), (LivingEntity) (Object) this);
+        damageContainers.peek().setNewDamage(newDamage);
+        return newDamage;
     }
 }
--- src/main/java/com/shieldmechanics/mixin/PlayerHurtMixin.java
+++ src/main/java/com/shieldmechanics/mixin/PlayerHurtMixin.java
@@ -2,18 +2,27 @@ package com.shieldmechanics.mixin;
 
 import com.shieldmechanics.event.EventHandler;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Player.class)
-public class PlayerHurtMixin
+public abstract class PlayerHurtMixin extends LivingEntity
 {
+    protected PlayerHurtMixin(final EntityType<? extends LivingEntity> p_20966_, final Level p_20967_)
+    {
+        super(p_20966_, p_20967_);
+    }
+
     @ModifyVariable(method = "actuallyHurt", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private float onHurt(final float dmg, final DamageSource damageSource, final float f)
     {
-        return EventHandler.onEntityHurt(damageSource, dmg, (LivingEntity) (Object) this);
+        final float newDamage = EventHandler.onEntityHurt(damageSource, damageContainers.peek().getNewDamage(), (LivingEntity) (Object) this);
+        damageContainers.peek().setNewDamage(newDamage);
+        return newDamage;
     }
 }
